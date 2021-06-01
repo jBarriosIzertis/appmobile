597f58d6ebca1ed3b24d39d34d7c434ed137f56a
jBarriosIzertis <85111050+jBarriosIzertis@users.noreply.github.com>
2021-06-01 10:28:16 +0200

    Create _callback.php

---
+++ _callback.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta name="salesforce-community" content="https://loginwidget-developer-edition.mobile1.t.force.com/loginwidget">
+<meta name="salesforce-mode" content="modal-callback">
+<meta name="salesforce-save-access-token" content="true">
+<meta name="salesforce-allowed-domains" content="embeddedlogin.herokuapp.com">
+<script src="https://loginwidget-developer-edition.mobile1.t.force.com/loginwidget/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+</head>
+</html>
