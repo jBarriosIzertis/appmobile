12b2a312974920dbcfaa69af515fc04107eefb1a
jBarriosIzertis <85111050+jBarriosIzertis@users.noreply.github.com>
2021-06-01 10:29:30 +0200

    Update _callback.php

--- _callback.php
+++ _callback.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<meta name="salesforce-community" content="https://loginwidget-developer-edition.mobile1.t.force.com/loginwidget">
+<meta name="salesforce-community" content="https://uat-bergec.cs129.force.com/AppMovil">
 <meta name="salesforce-mode" content="modal-callback">
 <meta name="salesforce-save-access-token" content="true">
-<meta name="salesforce-allowed-domains" content="embeddedlogin.herokuapp.com">
-<script src="https://loginwidget-developer-edition.mobile1.t.force.com/loginwidget/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+<meta name="salesforce-allowed-domains" content="appmobilemule.herokuapp.com">
+<script src="https://uat-bergec.cs129.force.com/AppMovil/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 </head>
 </html>
